ae6056c9a43d60174735dc80112d861470f81b8c
mccampero <mccampero2@gmail.com>
2024-06-24 10:32:14 -0400

    impliment git show logic

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+
+// server.js
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require("method-override"); // new
+const morgan = require("morgan"); //new
+
+// DB connection code
+
+// Mount it along with our other middleware, ABOVE the routes
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
+
+// routes below
 const dotenv = require("dotenv"); // require package
 dotenv.config(); 
 const express = require("express");
@@ -21,11 +36,26 @@ app.use(express.urlencoded({ extended: false }));
     res.render("index.ejs");
   });
 
+  // GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  console.log(allFruits);
+  res.render('fruits/index.ejs', { fruits: allFruits });
+});
+
+
 //Get fruits new/ 
  app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
   });
 
+  app.get("/fruits/:fruitId", async (req, res) => {
+   const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('fruits/show.ejs', {  fruit: foundFruit });
+    
+  });
+  
+  
   // POST /fruits
 app.post("/fruits", async (req, res) => {
     if (req.body.isReadyToEat === "on") {
@@ -34,7 +64,7 @@ app.post("/fruits", async (req, res) => {
         req.body.isReadyToEat = false;
       }
       await Fruit.create(req.body);
-    res.redirect("/fruits/new");
+    res.redirect("/fruits");
   });
 
 app.listen(3000, () => {
